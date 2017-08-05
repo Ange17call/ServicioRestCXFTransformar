ccdd607eefe423031d46447177a5a7c6c09ebe33
Angel Calero <fercho_17cal@hotmail.com>
2017-08-05 11:08:44 -0500

    primer commit

---
+++ transformarAMayusculas/src/com/distribuida/implementaciones/ServicioTransformarImpl.java
@@ -0,0 +1,25 @@
+package com.distribuida.implementaciones;
+
+
+import javax.ws.rs.Consumes;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+import org.springframework.stereotype.Component;
+
+import com.distribuida.interfaces.ServicioTransformar;
+
+@Component(value="servicioTransformar")
+public class ServicioTransformarImpl implements ServicioTransformar {
+	
+	@GET
+	@Path("/transformar/{texto}")
+	@Consumes({ MediaType.APPLICATION_JSON })
+	@Produces({ MediaType.APPLICATION_JSON })
+	public String transformar(@PathParam(value="texto") String texto){
+		return texto.toUpperCase();
+	}
+}
