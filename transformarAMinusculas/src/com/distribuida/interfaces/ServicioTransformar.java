0248bde13ed39d8d8be971884830d26df3ccd256
Angel Calero <fercho_17cal@hotmail.com>
2017-08-05 12:12:49 -0500

    segundo commit se cambio @PathParam por @QueryParam

--- transformarAMinusculas/src/com/distribuida/interfaces/ServicioTransformar.java
+++ transformarAMinusculas/src/com/distribuida/interfaces/ServicioTransformar.java
@@ -3,8 +3,8 @@ package com.distribuida.interfaces;
 import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
-import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
+import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.MediaType;
 
 
@@ -12,10 +12,10 @@ public interface ServicioTransformar {
 	
 
 	 @GET
-	 @Path("/transformar/{texto}")
+	 @Path("/transformar")
 	 @Consumes({ MediaType.APPLICATION_JSON })
 	 @Produces({ MediaType.APPLICATION_JSON })
-	 public String transformar(@PathParam(value="texto") String texto);
+	 public String transformar(@QueryParam(value="texto") String texto);
 	
 	 
 	 
