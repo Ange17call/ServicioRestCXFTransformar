cd66f6d058666dc8f56fb74562bcd79ffd804df4
Angel Calero <fercho_17cal@hotmail.com>
2017-08-05 12:16:04 -0500

    segundo commit se cambio @PathParam por @QueryParam

--- transformarAReversa/src/com/distribuida/implementaciones/ServicioTransformarImpl.java
+++ transformarAReversa/src/com/distribuida/implementaciones/ServicioTransformarImpl.java
@@ -4,8 +4,8 @@ package com.distribuida.implementaciones;
 import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
-import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
+import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.MediaType;
 
 import org.springframework.stereotype.Component;
@@ -16,10 +16,10 @@ import com.distribuida.interfaces.ServicioTransformar;
 public class ServicioTransformarImpl implements ServicioTransformar {
 	
 	@GET
-	@Path("/transformar/{texto}")
+	@Path("/transformar")
 	@Consumes({ MediaType.APPLICATION_JSON })
 	@Produces({ MediaType.APPLICATION_JSON })
-	public String transformar(@PathParam(value="texto") String texto){
+	public String transformar(@QueryParam(value="texto") String texto){
 		
 		StringBuilder sb = new StringBuilder( texto );
 		return sb.reverse().toString( );
